d63be2a5f1cce74afe48d4e76bede58d2eac576a
Aamanku <55757512+aamanku@users.noreply.github.com>
2020-01-08 21:21:45 +0530

    RPM meter
    Detecting current pulses at spark distributor to deduce RPM of engine

---
+++ arduinorpm.ino
@@ -0,0 +1,32 @@
+
+volatile byte counter;
+unsigned int rpm;
+unsigned long timehold;
+unsigned long lastcounttime;
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(2,INPUT);
+  counter=1;
+  rpm=0;
+  timehold=0;
+  lastcounttime=0;
+  attachInterrupt(2,isr,RISING);
+  }
+void loop()
+{
+  if((millis()-timehold)>=1000){
+  detachInterrupt(2);
+  rpm=20*counter;
+  Serial.println(rpm);
+  timehold=millis();
+  counter=0;
+  attachInterrupt(2,isr,RISING);
+  }}
+void isr()
+{
+  if((millis()-lastcounttime)>10){
+  counter++;
+  lastcounttime=millis();
+  }
+  }
